40884f54b1df2cf0b811e4ea3a2b8398fc044553
Luis Alberto Cisneros Gómez <tiko.cisneros@gmail.com>
2024-04-14 12:56:22 -0500

    refactor: improve getTrends api request

--- services/api.ts
+++ services/api.ts
@@ -3,16 +3,15 @@ import { MediaType, ApiResult } from '~/models';
 const API_KEY = process.env.EXPO_PUBLIC_API_KEY;
 const BASE_URL = process.env.EXPO_PUBLIC_API_URL;
 
-export const getTrends = async (
-  mediaType: MediaType = MediaType.MOVIE,
-  page: number = 1
-): Promise<ApiResult> => {
-  const response = await fetch(
-    `${BASE_URL}/trending/${mediaType}/day?language=en-US&api_key=${API_KEY}&page=${page}`
-  );
-  const data = await response.json();
-  return data;
-};
+export const getTrends =
+  (page: number = 1) =>
+  async (): Promise<ApiResult> => {
+    const response = await fetch(
+      `${BASE_URL}/trending/all/day?language=en-US&api_key=${API_KEY}&page=${page}`
+    );
+    const data = await response.json();
+    return data;
+  };
 
 export const getSearchResults = async (query: string): Promise<ApiResult> => {
   const response = await fetch(
